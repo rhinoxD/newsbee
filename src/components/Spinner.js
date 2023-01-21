b3b90228650d4c9514e3215150ec62751eba6f7f
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-21 11:17:47 +0530

    Refactor to app based component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
 import loading from './loading.gif'
-// import loading from './6SSp.gif'
-// import loading from './2yqT.gif'
-// import loading from './2unv.gif'
 
-export class Spinner extends Component {
-  render() {
-    return (
-      <div className='text-center'>
-        <img className='my-3' src={loading} alt='loading' style={{ width: '3%' }} />
-      </div>
-    )
-  }
+const Spinner = () => {
+  return (
+    <div className='text-center'>
+      <img
+        className='my-3'
+        src={loading}
+        alt='loading'
+        style={{ width: '3%' }}
+      />
+    </div>
+  )
 }
 
 export default Spinner
